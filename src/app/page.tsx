0a9edb426797a13259a9eec407297601da36b1aa
MichiMauch <michi.mauch@gmail.com>
2024-10-21 20:04:55 +0200

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+"use client";
+import React, { useEffect, useState, useCallback } from 'react';
 import BentoGrid from '../components/CardComponent'; // Importiere die BentoGrid-Komponente
 import Pager from '../components/Pager'; // Importiere die Pager-Komponente
 import Header from '../components/Header'; // Pfad anpassen
 import Footer from '../components/Footer'; // Importiere den Footer
 import Spinner from '../components/Spinner'; // Importiere die Spinner-Komponente
-import { getSheetData } from '../../lib/googleSheets'; // Importiere deine Funktion für den Datenabruf
 
 export interface Place {
   title: string;
@@ -18,42 +18,24 @@ export interface Place {
   country_code: string;
 }
 
-interface HomeProps {
-  places: Place[];
-}
-
-export default function Home({ places }: HomeProps) {
+export default function Home() {
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [filteredData, setFilteredData] = useState<Place[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isClient, setIsClient] = useState(false); // Zustand, um zu erkennen, ob wir im Client sind
   const [currentPage, setCurrentPage] = useState(1); // Aktuelle Seite für Pagination
   const itemsPerPage = 14; // Anzahl der Elemente pro Seite
 
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+  useEffect(() => {
+    // Markiere den Client, sobald der Code im Browser ausgeführt wird
+    setIsClient(true);
+  }, []);
+
+  const convertDate = (dateStr: string) => {
+    const [day, month, year] = dateStr.split('.').map(Number);
+    return new Date(year, month - 1, day);
   };
 
-  const paginatedData = places.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className="flex-grow">
-        <BentoGrid places={paginatedData} />
-        <Pager
-          currentPage={currentPage}
-          totalItems={places.length}
-          itemsPerPage={itemsPerPage}
-          onPageChange={handlePageChange}
-        />
-      </div>
-      <Footer />
-    </div>
-  );
-}
-
-// Verwende getStaticProps, um Daten serverseitig zu laden
-export async function getStaticProps() {
-  const rows = await getSheetData();
-
-  // Verarbeite die Daten, um das Format für deine Komponente zu erstellen
   const convertCloudflareLink = (link: string) => {
     const segments = link.split('/');
     const filename = segments[segments.length - 1];
@@ -61,24 +43,71 @@ export async function getStaticProps() {
     return baseName ? `https://pub-7b46ce1a4c0f4ff6ad2ed74d56e2128a.r2.dev/${baseName}.webp` : '';
   };
 
-  const formattedData: Place[] = rows.map((row: any[]) => ({
-    title: row[0],
-    location: row[1],
-    dateFrom: row[2],
-    dateTo: row[3],
-    imageLinks: row[4] ? convertCloudflareLink(row[4]) : '',
-    latitude: parseFloat(row[5]),
-    longitude: parseFloat(row[6]),
-    country: row[7],
-    country_code: row[8],
-  }));
+  const fetchData = useCallback(() => {
+    fetch('/api/sheetData') // Die korrekte API für das Laden der Daten
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Netzwerkantwort war nicht ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const formattedData: Place[] = data.map((row: any[]) => ({
+          title: row[0],
+          location: row[1],
+          dateFrom: row[2],
+          dateTo: row[3],
+          imageLinks: row[4] ? convertCloudflareLink(row[4]) : '',
+          latitude: parseFloat(row[5]),
+          longitude: parseFloat(row[6]),
+          country: row[7],
+          country_code: row[8],
+        }));
+        const sortedData = formattedData.sort((a, b) => convertDate(b.dateFrom).getTime() - convertDate(a.dateFrom).getTime());
+        setPlaces(sortedData);
+        setFilteredData(sortedData); // Initialize filteredData with all data
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Fehler beim Abrufen der Daten:', error);
+        setIsLoading(false);
+      });
+  }, []);
 
-  const sortedData = formattedData.sort((a, b) => new Date(b.dateFrom).getTime() - new Date(a.dateFrom).getTime());
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-  return {
-    props: {
-      places: sortedData, // übergib die Daten an die Komponente
-    },
-    revalidate: 600, // ISR: Seite wird alle 10 Minuten neu generiert
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
   };
+
+  const paginatedData = filteredData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  // Verhindere das Rendern des UI im Serverzustand, bis der Client erkannt wurde
+  if (!isClient) {
+    return <div className="min-h-screen flex flex-col justify-between" />; // Leeres Div um Sprünge zu vermeiden
+  }
+
+  // Zeige den Spinner an, während die Daten geladen werden
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  // Wenn alles geladen ist, zeige den eigentlichen Inhalt
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">
+        <BentoGrid places={paginatedData} />
+        <Pager
+          currentPage={currentPage}
+          totalItems={filteredData.length}
+          itemsPerPage={itemsPerPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+      <Footer /> {/* Footer immer unten fixiert */}
+    </div>
+  );
 }
